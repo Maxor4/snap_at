ce4eba64bffb35636d47db5c7325c19d1b34e630
piresm <maxime.pires@outlook.com>
2017-10-04 14:42:41 +0200

    Stylisation

--- Snap_At/scripts/Couleurs.js
+++ Snap_At/scripts/Couleurs.js
@@ -5,43 +5,25 @@ var Couleurs = {
     font: 'MyriadPro-Regular',
 
 
-    primaire: '#2980b9',
-    secondaire: '#00A987',
-    texteSurFond: '#FFFFFF',
-
-    placeholder: '#B2B2B2',
-
-    //Discussions
-    fondDiscussion: '#E9EAEB',
-    fondBulleRobot: '#FFFFFF',
-    fondCarousel: '#FFFFFF',
-
-    //Propositions
-    bandeauProposition: '#FFFFFF',
-    bandeauBordureBouton: '#EAEAEA',
-    bandeauPropositionShadowColor: '#B9B9B9',
-
-    //Page d'accueil
-    bleuAccueil: '#3C98DB',
-    titrePrincipal: '#46535D',
-
-    //Détails
-    propositions: {
-        protocoles: {
-            fond: '#E3E3E3'
-        },
-        patient: {
-            couleurTitre: '#3A89E9'
-        }
+    header :{
+        background: '#2F3649',
+        title: '#FF8929'
     },
 
-    //Page Souscriptions
-    fondTransparent:'#1E1E1E',
-    boutonPagination: '#1a1917',
+    mainColors :{
+      orange: '#FF8929',
+      black: '#2F3649'
+    },
+
+    list:{
+      background: '#F5FCFF',
+        border: '#B9B9B9',
+    },
 
     blanc: '#FFFFFF',
     rouge: '#900',
     bleuClair: '#00aeef',
+    noir: '#000',
 
 };
 
